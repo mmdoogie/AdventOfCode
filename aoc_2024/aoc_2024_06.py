f95474e383d60afeb63076e3b239f2f3c84a7db0
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2024-12-06 17:00:35 -0600

    Remove some redundancies in 2024 Day 6 Part 2 algorithm

--- aoc_2024/aoc_2024_06.py
+++ aoc_2024/aoc_2024_06.py
@@ -10,57 +10,65 @@ def parse():
 def get_visited(grid, start):
     loc = start
     heading = cpt.UP
-    visited = set()
+    visited = {(loc, heading): True}
     while True:
         if loc not in grid:
             return visited
-        visited.add(loc)
         loc += heading
         if loc not in grid:
             return visited
         if grid[loc] == '#':
             loc -= heading
             heading = cpt.right_turn(heading)
+            continue
+        visited[(loc, heading)] = True
 
 def part1(output=False):
     lines = parse()
     grid = cpt.grid_as_dict(lines)
     start = [k for k, v in grid.items() if v == '^'][0]
-    visited = get_visited(grid, start)
+    visited = set(k[0] for k in get_visited(grid, start))
     if output:
         grid = {cpt.as_xy(k, int): v for k, v in grid.items()}
         img.print_image(grid, use_char=True, highlighter=lambda x, y, c: ansi.red('*') if cpt.from_xy(x, y) in visited else c)
     return len(visited)
 
-def check_loop(grid, start, addin, max_loop):
-    loc = start
-    heading = cpt.UP
+def check_loop(grid, start, pre_visited, addin):
+    loc = start[0]
+    heading = start[1]
     visited = set()
-    old_points = 0
     while True:
         if loc not in grid:
             return False
-        if loc in visited:
-            old_points += 1
-        else:
-            old_points = 0
-            visited.add(loc)
         loc += heading
         if loc not in grid:
             return False
         if loc == addin or grid[loc] == '#':
             loc -= heading
             heading = cpt.right_turn(heading)
-        if old_points > max_loop:
+            continue
+        if (loc, heading) in visited or (loc, heading) in pre_visited:
             return True
+        visited.add((loc, heading))
 
 def part2(output=False):
     lines = parse()
     grid = cpt.grid_as_dict(lines)
     start = [k for k, v in grid.items() if v == '^'][0]
-    visited = get_visited(grid, start)
-    perim = (len(lines) + len(lines[0])) * 2
-    return sum(check_loop(grid, start, v, perim) for v in visited)
+    visited = list(get_visited(grid, start).keys())
+    visited_points = [k[0] for k in visited]
+
+    obstrs = {vp: visited_points.index(vp) for vp in visited_points}
+    total = 0
+    visited_so_far = set()
+    vsf_idx = -1
+    for ob_pt, ob_idx in obstrs.items():
+        if ob_pt == start:
+            continue
+        visited_so_far.update(visited[vsf_idx:ob_idx])
+        vsf_idx = ob_idx
+        total += check_loop(grid, visited[ob_idx - 1], visited_so_far, ob_pt)
+    return total
 
 if __name__ == '__main__':
     print('Part 1:', part1(True))
